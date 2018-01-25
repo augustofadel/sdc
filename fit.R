263adc8ba027a24cabe6d5ca0956aed767bc9464
augustofadel <augustofadel@gmail.com>
2018-01-25 18:47:03 -0200

    IL2_r included as default

--- fit.R
+++ fit.R
@@ -1,6 +1,6 @@
 # fitness -----------------------------------------------------------------
 
-fit <- function(dat, clus, metricas = c('DLD', 'SDID', 'IL1', 'IL2', 'IL3')) {
+fit <- function(dat, clus, metricas = c('DLD', 'SDID', 'IL1', 'IL2', 'IL2_r', 'IL3')) {
    dat.agreg <- agreg(dat, clus)
    fit.vec <- sapply(metricas, function(x) do.call(x, list(dat, dat.agreg)))
    return(fit.vec)
