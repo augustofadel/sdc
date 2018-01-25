1a3f239c1ffc75ed4af820c64d9e45940e986eee
augustofadel <augustofadel@gmail.com>
2018-01-25 18:45:46 -0200

    Equivalent to IL1 from Mateo-Sanz et al. (2004)

--- IL2.R
+++ IL2.R
@@ -1,15 +1,11 @@
-# Information Loss: IL2 ---------------------------------------------------
+# Information Loss: IL2 [Mateo-Sanz et al. (2004)] ------------------------
 
 IL2 <- function(dat, dat.agreg) {
+   denom <- sqrt(2) * as.numeric(dat[, lapply(.SD, sd)]) #dat normalizado, sd = 1 para toda vairavel
    IL <-
-      (abs(dat - dat.agreg) / sqrt(2)) %>% #dat normalizado, apply(dat, 2, sd) = 1
+      abs(dat - dat.agreg) %>% 
+      apply(1, function(x) {x / denom}) %>% 
       apply(2, sum) %>% 
-      sum() / prod(dim(dat))
-   # return(IL)
-   IL_max <-
-      sweep(dat.agreg, 2, as.numeric(dat[, lapply(.SD, mean)])) %>%
-      abs() %>%
-      apply(2, sum) %>%
-      sum() / prod(dim(dat))
-   return(IL / IL_max * 100)
+      sum() / ncol(dat)
+   return(IL)
 }
