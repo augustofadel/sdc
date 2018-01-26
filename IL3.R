b85c6b4ab28c03b6a6e09e0608fb58dd7858b96b
augustofadel <augustofadel@gmail.com>
2018-01-26 10:55:08 -0200

    Revised according to Domingo-Ferrer e Torra (2001)

--- IL3.R
+++ IL3.R
@@ -1,4 +1,5 @@
 # Information Loss: IL3 ---------------------------------------------------
+# Domingo-Ferrer e Torra (2001) p.8
 
 IL3 <- function(dat, dat.agreg) {
    n <- nrow(dat)
@@ -11,32 +12,34 @@ IL3 <- function(dat, dat.agreg) {
       sum() / (n * p)
    
    dat.mean <- dat[, lapply(.SD, mean)]
+   dat.agreg.mean <- dat.agreg[, lapply(.SD, mean)]
    
-   IL[2] <- sum(
-      abs(dat.mean - dat.agreg[, lapply(.SD, mean)]) / abs(dat.mean)
-   ) / p
+   IL[2] <- # verificar a necesssidade desse termo
+      (abs(dat.mean - dat.agreg.mean) / abs(dat.mean)) %>% 
+      sum() %>% 
+      `/`(p)
    
    dat.cov <- cov(dat)
    dat.agreg.cov <- cov(dat.agreg)
    triang.sup <- upper.tri(dat.cov, diag = T)
    
    IL[3] <- 
-      sum(
-         abs(dat.cov[triang.sup] - dat.agreg.cov[triang.sup]) / abs(dat.cov[triang.sup])
-      ) / ((p * (p + 1))/2)
+      (abs(dat.cov[triang.sup] - dat.agreg.cov[triang.sup]) / abs(dat.cov[triang.sup])) %>% 
+      sum() %>% 
+      `/`((p * (p + 1))/2)
    
    IL[4] <- 
-      sum(
-         abs(diag(dat.cov) - diag(dat.agreg.cov)) #dat normalizado
-      ) / p
+      (abs(diag(dat.cov) - diag(dat.agreg.cov)) / diag(dat.cov)) %>% #dat normalizado
+      sum() %>% 
+      `/`(p)
    
    dat.cor <- cor(dat)
    dat.agreg.cor <- cor(dat.agreg)
    
    IL[5] <- 
-      sum(
-         abs(dat.cor[triang.sup] - dat.agreg.cor[triang.sup])
-      ) / ((p * (p + 1))/2)
+      (abs(dat.cor[triang.sup] - dat.agreg.cor[triang.sup])) %>% 
+      sum() %>% 
+      `/`((p * (p + 1))/2)
    
    return(sum(IL) / 5)
 }
