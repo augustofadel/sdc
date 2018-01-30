6a7bfdda7b52586eb10b50627be087bbc0781e34
augustofadel <augustofadel@gmail.com>
2018-01-30 12:57:53 -0200

    Data frame suitable version

--- IL2.R
+++ IL2.R
@@ -1,11 +1,20 @@
 # Information Loss: IL2 [Mateo-Sanz et al. (2004)] ------------------------
 
 IL2 <- function(dat, dat.agreg) {
-   denom <- sqrt(2) * as.numeric(dat[, lapply(.SD, sd)]) #dat normalizado, sd = 1 para toda vairavel
-   IL <-
+   # denom <- sqrt(2) * as.numeric(dat[, lapply(.SD, sd)]) #dat normalizado, sd = 1 para toda vairavel
+   # IL <-
+   #    abs(dat - dat.agreg) %>% 
+   #    apply(1, function(x) {x / denom}) %>% 
+   #    apply(2, sum) %>% 
+   #    sum() / prod(dim(dat))
+   
+   Sj <- apply(dat, 2, sd)
+   IL <- 
       abs(dat - dat.agreg) %>% 
-      apply(1, function(x) {x / denom}) %>% 
+      `/`(sqrt(2) * Sj) %>% 
       apply(2, sum) %>% 
-      sum() / ncol(dat)
+      sum() %>% 
+      `/`(prod(dim(dat)))
+
    return(IL)
 }
